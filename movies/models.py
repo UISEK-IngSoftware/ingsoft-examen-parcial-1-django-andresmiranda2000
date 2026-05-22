153099c6af371800b8a8b990108fee61c2f89ae6
Andrés Miranda <amiranda20132013@gmail.com>
2026-05-22 16:03:14 -0500

    Diseño y Correcciones
    Se ha cambiado el diseño inspirado en Blockbuster.

--- movies/models.py
+++ movies/models.py
@@ -7,6 +7,7 @@ class Movie(models.Model):
 	director = models.CharField(max_length=120)
 	publication_year = models.IntegerField()
 	synopsis = models.TextField()
+	poster_url = models.URLField(blank=True)
 
 	def __str__(self):
 		return f"{self.title} - {self.publication_year}"
